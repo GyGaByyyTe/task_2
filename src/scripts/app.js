74592ac5a5dc24ffeeb8495e4a81388e5254f0b6
GyGaByyyTe <gygabyyyte@gmail.com>
2018-08-11 21:04:41 +0300

    fixed slider layout

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,17 +2,19 @@ document.addEventListener(
   'DOMContentLoaded',
   function() {
     var overlay__slider = document.getElementsByClassName('overlay__slider');
-    var slider = document.getElementsByClassName('slider');
+    var sliders = document.getElementsByClassName('slider');
 
     var setSliderWidth = function() {
-      var paddingTop = parseInt(
-        getComputedStyle(overlay__slider[0]).paddingTop
-      );
-      var paddingBottom = parseInt(
-        getComputedStyle(overlay__slider[0]).paddingBottom
-      );
-      slider[0].style.width =
-        overlay__slider[0].clientHeight - (paddingTop + paddingBottom) + 'px';
+      for (var i = 0; i < sliders.length; i++) {
+        var paddingTop = parseInt(
+          getComputedStyle(overlay__slider[i]).paddingTop
+        );
+        var paddingBottom = parseInt(
+          getComputedStyle(overlay__slider[i]).paddingBottom
+        );
+        sliders[i].style.width =
+          overlay__slider[i].clientHeight - (paddingTop + paddingBottom) + 'px';
+      }
     };
 
     window.addEventListener(
@@ -24,6 +26,20 @@ document.addEventListener(
     );
 
     setSliderWidth();
+
+    var overlayButtons = document.getElementsByClassName('overlay__button');
+    var overlays = document.getElementsByClassName('overlay');
+    var contentBlock = document.getElementById('content');
+
+    for (var i = 0; i < overlayButtons.length; i++) {
+      overlayButtons[i].addEventListener('click', function(ev) {
+        ev.preventDefault();
+        for (var j = 0; j < overlays.length; j++) {
+          overlays[j].style.display = 'none';
+        }
+        contentBlock.classList.toggle('content--blur');
+      });
+    }
   },
   false
 );
